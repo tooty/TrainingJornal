7605f8bc39f9bddcb6e531de1f4f395623cc0e0e
Thomas <ttichy@hotmail.de>
2023-09-07 09:30:54 +0200

    Initial Commit

---
+++ TrainingMulti/TrainingMultiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainingMultiApp.swift
+//  TrainingMulti
+//
+//  Created by Thomas Tichy on 07.09.23.
+//
+
+import SwiftUI
+
+@main
+struct TrainingMultiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
